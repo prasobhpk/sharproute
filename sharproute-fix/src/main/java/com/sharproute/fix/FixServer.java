6a833cc7ff4bf9a8581fe4b74d4ad7b443cda80e
Stephen Nimmo <stephennimmo@gmail.com>
2013-01-30 16:02:45 -0600

    Updated to move stuff to FixSessionManager

--- sharproute-fix/src/main/java/com/sharproute/fix/FixServer.java
+++ sharproute-fix/src/main/java/com/sharproute/fix/FixServer.java
@@ -45,6 +45,9 @@ public class FixServer implements ApplicationContextAware, EntryListener<Integer
 	
 	private static Logger logger = LoggerFactory.getLogger(FixServer.class);
 	
+	@Resource
+	private FixSessionManager fixSessionManager;
+	
 	private ApplicationContext applicationContext;
 	
 	@Value("${fixEngine.uid}")
@@ -85,66 +88,15 @@ public class FixServer implements ApplicationContextAware, EntryListener<Integer
     	IMap<Integer, FixSession> fixSessionMap = hazelcastInstance.getMap(FixSession.class.getSimpleName());
     	fixSessionMap.addEntryListener(this, true);
     	for (Map.Entry<Integer, FixSession> entry : fixSessionMap.entrySet(new SqlPredicate("fixEngine.uid = " + fixEngineUid))) {
-			FixSession fixSession = entry.getValue();
-			SessionID sessionID = new SessionID(fixSession.getBeginString(), fixSession.getSenderCompId(), fixSession.getTargetCompId());
-			Application application = createApplication();
-			MessageStoreFactory messageStoreFactory = createMessageStoreFactory();
-			SessionSettings sessionSettings = createSessionSettings(fixSession);
-			LogFactory logFactory = new ScreenLogFactory();
-			MessageFactory messageFactory = new DefaultMessageFactory();
-			Acceptor acceptor = new ThreadedSocketAcceptor(application, messageStoreFactory, sessionSettings, logFactory, messageFactory);
-			connectorMap.put(sessionID, acceptor);
+    		FixSession fixSession = entry.getValue();
+			Connector connector = fixSessionManager.createConnector(fixSession);
+			connectorMap.put(connector.getSessions().get(0), connector);
 		} 
     }
     
-    private Application createApplication(){
-    	return applicationContext.getBean("defaultApplication", DefaultApplication.class);
-    }
-    
-    private MessageStoreFactory createMessageStoreFactory(){
-    	return new MemoryStoreFactory();
-    }
-    
-    private SessionSettings createSessionSettings(FixSession fixSession){
-    	SessionSettings sessionSettings = new SessionSettings();
-    	SessionID sessionID = new SessionID(new BeginString(fixSession.getBeginString()), new SenderCompID(fixSession.getSenderCompId()), new TargetCompID(fixSession.getTargetCompId()));
-    	sessionSettings.setString(sessionID, SessionSettings.BEGINSTRING, fixSession.getBeginString());
-    	sessionSettings.setString(sessionID, quickfix.SessionSettings.SENDERCOMPID, fixSession.getSenderCompId());
-    	sessionSettings.setString(sessionID, quickfix.SessionSettings.TARGETCOMPID, fixSession.getTargetCompId());
-    	sessionSettings.setString(sessionID, quickfix.DefaultSessionFactory.SETTING_CONNECTION_TYPE, fixSession.getConnectionType().toString().toLowerCase());
-    	if (fixSession.getConnectionType().equals(ConnectionType.ACCEPTOR)){
-    		sessionSettings.setString(sessionID, quickfix.Acceptor.SETTING_SOCKET_ACCEPT_ADDRESS, fixSession.getHost());
-    		sessionSettings.setLong(sessionID, quickfix.Acceptor.SETTING_SOCKET_ACCEPT_PORT, fixSession.getPort());
-    	} else {
-    		sessionSettings.setString(sessionID, quickfix.Initiator.SETTING_SOCKET_CONNECT_HOST, fixSession.getHost());
-    		sessionSettings.setLong(sessionID, quickfix.Initiator.SETTING_SOCKET_CONNECT_PORT, fixSession.getPort());
-    	}
-
-    	//TODO Hardcodes
-    	sessionSettings.setLong(sessionID, quickfix.Session.SETTING_HEARTBTINT, 30);
-    	sessionSettings.setLong(sessionID, "ReconnectInterval", 60);
-    	sessionSettings.setString(sessionID, quickfix.Session.SETTING_USE_DATA_DICTIONARY, "N");
-    	sessionSettings.setString(sessionID, "StartTime", "19:30:00 US/Eastern");
-    	sessionSettings.setString(sessionID, "EndTime", "18:33:00 US/Eastern");
-    	
-    	return sessionSettings;
-    }
-    
-    private void addNewConnection(FixSession fixSession) {
-		SessionID sessionID = new SessionID(fixSession.getBeginString(), fixSession.getSenderCompId(), fixSession.getTargetCompId());
-		Application application = createApplication();
-		MessageStoreFactory messageStoreFactory = createMessageStoreFactory();
-		SessionSettings sessionSettings = createSessionSettings(fixSession);
-		LogFactory logFactory = new ScreenLogFactory();
-		MessageFactory messageFactory = new DefaultMessageFactory();
-		Acceptor acceptor;
-		try {
-			acceptor = new ThreadedSocketAcceptor(application, messageStoreFactory, sessionSettings, logFactory, messageFactory);
-			connectorMap.put(sessionID, acceptor);
-		} catch (ConfigError e) {
-			//TODO Handle configerror better
-			logger.error(e.getMessage());
-		}
+    private void addNewConnection(FixSession fixSession) throws ConfigError {
+		Connector connector = fixSessionManager.createConnector(fixSession);
+		connectorMap.put(connector.getSessions().get(0), connector);
     }
     
     private void removeConnection(FixSession fixSession){
@@ -153,11 +105,11 @@ public class FixServer implements ApplicationContextAware, EntryListener<Integer
     	connector.stop();
     }
     
-    private void updateConnection(FixSession fixSession){
+    private void updateConnection(FixSession fixSession) throws ConfigError {
     	SessionID sessionID = new SessionID(fixSession.getBeginString(), fixSession.getSenderCompId(), fixSession.getTargetCompId());
     	Connector connector = connectorMap.get(sessionID);
     	connector.stop();
-    	connector = createConnector(fixSession);
+    	connector = fixSessionManager.createConnector(fixSession);
     	try {
 			connector.start();
 		} catch (RuntimeError | ConfigError e) {
@@ -167,25 +119,14 @@ public class FixServer implements ApplicationContextAware, EntryListener<Integer
     	connectorMap.put(sessionID, connector);
     }
     
-    private Connector createConnector(FixSession fixSession){
-    	Application application = createApplication();
-		MessageStoreFactory messageStoreFactory = createMessageStoreFactory();
-		SessionSettings sessionSettings = createSessionSettings(fixSession);
-		LogFactory logFactory = new ScreenLogFactory();
-		MessageFactory messageFactory = new DefaultMessageFactory();
-		Acceptor acceptor = null;
-		try {
-			acceptor = new ThreadedSocketAcceptor(application, messageStoreFactory, sessionSettings, logFactory, messageFactory);
-		} catch (ConfigError e) {
-			//TODO Handle configerror better
-			logger.error(e.getMessage());
-		}
-		return acceptor;
-    }
-    
 	@Override
 	public void entryAdded(EntryEvent<Integer, FixSession> event) {
-		addNewConnection(event.getValue());
+		try {
+			addNewConnection(event.getValue());
+		} catch (ConfigError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -195,7 +136,12 @@ public class FixServer implements ApplicationContextAware, EntryListener<Integer
 
 	@Override
 	public void entryUpdated(EntryEvent<Integer, FixSession> event) {
-		updateConnection(event.getValue());
+		try {
+			updateConnection(event.getValue());
+		} catch (ConfigError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
